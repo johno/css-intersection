948bf65565c9b900e233f2c05111df8e4ed7a426
John Otander <johnotander@gmail.com>
2015-01-17 14:35:01 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var cssIntersection = require('..');
+
+describe('css-intersection', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(camelizedPackageName(), true);
+  });
+});
